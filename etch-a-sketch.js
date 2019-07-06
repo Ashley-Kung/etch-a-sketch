b1c51070d3ff2206ce43f8390922d67b3b56ccfc
Ashley Kung <ashley.berg@gmail.com>
2019-07-06 11:44:40 -0400

    Add button for dynamic grid size creation

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -2,10 +2,40 @@
 //starting out with grid 16x16. Then will make it dynamic with user input.
 
 //full grid is 800px x 800px.
-let cellWidth = 800/16 + 'px';
-console.log(cellWidth);
-let numCells = 16*16;
 
+let newGridButton = document.getElementById("new-grid");
+newGridButton.addEventListener('click', function(event) {
+  let gridContainer = document.getElementById("grid-container");
+
+  while (gridContainer.firstChild) {
+    gridContainer.removeChild(gridContainer.firstChild);
+  }
+  let squares = prompt('How many squares per side should the new grid be?');
+  let cellWidth = 800/squares + 'px';
+  console.log(cellWidth);
+  let numCells = squares*squares;
+
+
+  for (let i = 0; i < numCells; i++) {
+    let div = document.createElement('div');
+    div.id = i;
+    gridContainer.appendChild(div);
+  }
+  gridContainer.style.display = 'grid';
+  gridContainer.style.height = '800px';
+  gridContainer.style.width = '800px';
+  gridContainer.style.gridTemplateColumns = `repeat(${squares}, ${cellWidth})`
+
+  gridContainer.addEventListener('mouseover', function(event) {
+    event.target.style.backgroundColor = 'black';
+});
+
+
+
+});
+
+
+/*
 let gridContainer = document.getElementById("grid-container");
 for (let i = 0; i < numCells; i++) {
   let div = document.createElement('div');
@@ -20,6 +50,7 @@ gridContainer.style.gridTemplateColumns = `repeat(16, ${cellWidth})`
 gridContainer.addEventListener('mouseover', function(event) {
   event.target.style.backgroundColor = 'black';
 });
+*/
 //append divs
 
 //style as a grid
